abbfaa49659f3b72a0b2054565493e945fedf473
AranjeetaParihar <aparihar@deqode.com>
2024-12-25 22:03:02 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icons from "react-native-vector-icons/Ionicons";
+import {RFValue} from "react-native-responsive-fontsize"
+import Navigation from './src/navigation/Navigation';
+
+const App = () => {
+  return (
+    <Navigation/>
+  )
+}
+
+export default App
---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React, { FC } from 'react'
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import SplashScreen from '../screens/SplashScreen';
+import BaymaxScreen from '../screens/BaymaxScreen';
+
+const Stack = createNativeStackNavigator();
+const Navigation:FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='SplashScreen' screenOptions={{headerShown:false}}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+        <Stack.Screen name="BaymaxScreen" component={BaymaxScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Navigation
