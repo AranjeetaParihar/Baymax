6fd83c50edecb0871d3679b76773fb90f9238262
Aranjeeta <parihararanjeeta@gmail.com>
2025-04-13 17:52:36 +0530

    Splashscreen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,28 +1,69 @@
 import { View, Text, TouchableOpacity, StyleSheet, Animated, Image } from 'react-native'
-import React from 'react'
+import React, { FC, useEffect } from 'react'
 import { navigate } from '../utils/NavigationUtils'
 import { Colors, Fonts, lightColors } from '../utils/Constants'
 import { screenHeight, screenWidth } from '../utils/Scaling'
 import LinearGradient from "react-native-linear-gradient"
 import CustomText from '../components/global/CustomText'
 import LottieView from 'lottie-react-native';
+import { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 
 const bottomColor = [...lightColors].reverse()
-const SplashScreen = () => {
+const SplashScreen:FC= () => {
+
+  // initially at the very bottom
+  const baymaxAnimation = useSharedValue(80);
+  const messageAnimation = useSharedValue(screenHeight*0.8);
+
+  const launchAnimation = async() => {
+    messageAnimation.value = screenHeight*0.001;
+    setTimeout(()=>{
+      baymaxAnimation.value = -screenHeight*0.02;
+    },600)
+  }
+
+  // transform baymax
+  const animateImageStyle = useAnimatedStyle(()=>{
+    return {
+      transform:[{
+        // translateY: withTiming(baymaxAnimation.value, {duration:1500}),
+        translateY: baymaxAnimation.value,
+       
+      }]
+    }
+  })
+
+  const messageContainerStyle = useAnimatedStyle(()=>{
+    return {
+      transform:[{
+        translateY: withTiming(messageAnimation.value, {duration:1200}),
+       
+      }]
+    }
+  })
+
+  useEffect(() => {
+    console.log("value",baymaxAnimation.value);    
+  }, [baymaxAnimation.value]);
+  
+  useEffect(()=>{
+    // launchAnimation();
+  },[])
+
   return (
     <View style={style.container}>
-      <Animated.View style={style.imgContainer}>
-        <Image source={require("../assets/images/launch.png")} style={style.img}/>
+      <Animated.View style={[style.imgContainer,]}>
+        <Image source={require("../assets/images/launch.png")} style={[style.img,animateImageStyle]}/>
       </Animated.View>
 
-      <Animated.View style={style.gradientContainer}>
+      <Animated.View style={[style.gradientContainer,messageContainerStyle]}>
         <LinearGradient colors={bottomColor} style={style.gradient}>
           <View style={style.textContainer}>
             <CustomText fontSize={34} fontFamily={Fonts.Theme}>BAYMAX!</CustomText>
             <LottieView source={require("../assets/animations/sync.json")} 
             style={{width:300,height:100}} autoPlay={true} loop>
             </LottieView>
-            <CustomText>Synchronizing best configurations for you...</CustomText>
+            <CustomText fontSize={12}>Synchronizing best configurations for you...</CustomText>
           </View>
         </LinearGradient>
       </Animated.View>
